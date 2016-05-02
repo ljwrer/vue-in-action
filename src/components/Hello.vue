8481511e49ff3d7047d8e1a5d14bdf03523e21ec
ljwrer <ljwrer@sina.com>
2016-05-03 00:50:09 +0800

    computed data

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,13 +3,20 @@
     <h1>{{ msg }}</h1>
     <input v-model="msg" @keyup.enter="addTodo"/>
     <ul>
-      <li v-for="todo in todos"><span class="">{{$index+1}}.{{todo}}</span><button v-on:click="removeTodo($index)">delete</button></li>
+      <li v-for="todo in todos"><span class="">{{$index+1}}.{{todo}}</span><button @click="removeTodo($index)">delete</button></li>
     </ul>
     <button @click="reverseMessage">revese message</button>
     <p v-if="greeting">greeting !</p>
     <p>{{todos.length}}</p>
     <p>origin:{{*todos.length}}</p>
     <p>{{newMessage?"new:"+newMessage:"" | capitalize}}</p>
+    <a :href="url">index</a>
+    <p>{{computedMessage}}</p>
+    <input v-model="firstName">
+    <input v-model="lastName">
+    <p>{{getFullName}}</p>
+    <p>{{fullName}}</p>
+    <input v-model="fullName">
   </div>
 </template>
 
@@ -24,8 +31,14 @@ export default {
       msg: 'Hello World!',
       todos:["打开冰箱门","放进冰箱","关闭冰箱门"],
       greeting:true,
-      newMessage:null
+      newMessage:null,
+      url:"/",
+      firstName:"Ray",
+      lastName:"Zone"
     }
+  },
+  created(){
+    console.log(this);
   },
   methods:{
     reverseMessage(){
@@ -45,6 +58,25 @@ export default {
     create(){
       console.log(this);
     }
+  },
+  computed:{
+    computedMessage() {
+      return this.msg+" form computed"
+    },
+    getFullName() {
+      return this.firstName+" "+this.lastName;
+    },
+    fullName:{
+      get(){
+        return this.getFullName;
+      },
+      set(fullName){
+        var names=fullName.split(" ");
+        this.firstName=names[0]?names[0]:"";
+        this.lastName=names[names.length-1]?names[names.length-1]:"";
+      }
+    }
+
   }
 }
 </script>
