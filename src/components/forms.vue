208ec7373b93c47fd1fc82c2ae0773295484c146
ljwrer <ljwrer@sina.com>
2016-05-03 18:07:14 +0800

    form

--- src/components/forms.vue
+++ src/components/forms.vue
@@ -1,60 +1,62 @@
 <template>
-    <div>message:{{message}}</div>
-    <input type="text" v-model="message">
-    <div><input type="checkbox" v-model="checked">{{checked}}</div>
     <div>
-        <label for="good">good:</label> <input type="checkbox" id="good" value="good" v-model="checkedName">
-        <label for="better">better:</label><input type="checkbox" id="better" value="better" v-model="checkedName">
-        <label for="best">best:</label><input type="checkbox" id="best" value="best" v-model="checkedName">
-        <p>{{checkedName | json}}</p>
-    </div>
-    <div>
-        <input type="radio" id="one" value="One" v-model="picked">
-        <label for="one">One</label>
+        <div>message:{{message}}</div>
+        <input type="text" v-model="message">
+        <div><input type="checkbox" v-model="checked">{{checked}}</div>
+        <div>
+            <label for="good">good:</label> <input type="checkbox" id="good" value="good" v-model="checkedName">
+            <label for="better">better:</label><input type="checkbox" id="better" value="better" v-model="checkedName">
+            <label for="best">best:</label><input type="checkbox" id="best" value="best" v-model="checkedName">
+            <p>{{checkedName | json}}</p>
+        </div>
+        <div>
+            <input type="radio" id="one" value="One" v-model="picked">
+            <label for="one">One</label>
+            <br>
+            <input type="radio" id="two" value="Two" v-model="picked">
+            <label for="two">Two</label>
+            <br>
+            <span>Picked: {{ picked }}</span>
+        </div>
+        <div>
+            <select v-model="selectButton">
+                <option selected>A</option>
+                <option>B</option>
+                <option>C</option>
+            </select>
+            <p>{{selectButton}}</p>
+        </div>
+        <div>
+            <select v-model="selectButton2" multiple>
+                <option selected>A</option>
+                <option>B</option>
+                <option>C</option>
+            </select>
+            <p>{{selectButton2 | json}}</p>
+        </div>
+        <div>
+            <select v-model="selected">
+                <option v-for="option in options" :value="option.value">
+                    {{ option.text }}
+                </option>
+            </select>
+            <span>Selected: {{ selected }}</span>
+        </div>
+        <div><input type="checkbox" :true-value="trueVal" :false-value="falseVal" v-model="toggle"><span>{{toggle}}</span></div>
+        <div>
+            <input type="radio" :value="trueVal" v-model="radioButton" name="status">
+            <input type="radio" :value="falseVal" v-model="radioButton" name="status">
+            <span>{{radioButton}}</span>
+        </div>
+        <select v-model="selectedVal">
+            <option :value="{ number: 123 }">123</option>
+        </select>
         <br>
-        <input type="radio" id="two" value="Two" v-model="picked">
-        <label for="two">Two</label>
-        <br>
-        <span>Picked: {{ picked }}</span>
+        <input v-model="msg" lazy>
+        <p>{{msg}}</p>
+        <input v-model="age" number>
+        <input v-model="msg" debounce="2000">
     </div>
-    <div>
-        <select v-model="selectButton">
-            <option selected>A</option>
-            <option>B</option>
-            <option>C</option>
-        </select>
-        <p>{{selectButton}}</p>
-    </div>
-    <div>
-        <select v-model="selectButton2" multiple>
-            <option selected>A</option>
-            <option>B</option>
-            <option>C</option>
-        </select>
-        <p>{{selectButton2 | json}}</p>
-    </div>
-    <div>
-        <select v-model="selected">
-            <option v-for="option in options" :value="option.value">
-                {{ option.text }}
-            </option>
-        </select>
-        <span>Selected: {{ selected }}</span>
-    </div>
-    <div><input type="checkbox" :true-value="trueVal" :false-value="falseVal" v-model="toggle"><span>{{toggle}}</span></div>
-    <div>
-        <input type="radio" :value="trueVal" v-model="radioButton" name="status">
-        <input type="radio" :value="falseVal" v-model="radioButton" name="status">
-        <span>{{radioButton}}</span>
-    </div>
-    <select v-model="selectedVal">
-        <option :value="{ number: 123 }">123</option>
-    </select>
-    <br>
-    <input v-model="msg" lazy>
-    <p>{{msg}}</p>
-    <input v-model="age" number>
-    <input v-model="msg" debounce="2000">
 </template>
 <style scoped>
     *{
@@ -87,6 +89,9 @@
         },
         computed:{
 
+        },
+        ready(){
+            console.log(this.constructor)
         }
     }
 </script>
